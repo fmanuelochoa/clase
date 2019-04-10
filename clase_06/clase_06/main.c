45948bbaa59dc334138d2fb2e104e599316a647b
alumno <alumno@LAB00PC00>
2019-04-09 22:34:18 -0300

    push3

--- clase_06/clase_06/main.c
+++ clase_06/clase_06/main.c
@@ -50,9 +50,55 @@ int main()
     // }
 
      //int utn_getNumber(char *msg, char *msgEror,int minimo, int maximo,int reintentos,int *resultado)//se inicia con un getstring para validar
+    //  {
+      //    int retorno = -1;
+        //  char buffer[18];
+     //     int bufferInt;
+      //    //punteros no tienen que ser null
+        //  //minimo mas chico que maximo
+        //  if(valido todo )
+          //{
+    //         if(utn_getString(msg,msgError,1,16,reintentos,buffer)==0);
+   //           {
+    //              if(utn_isValidInt(buffer))
+      //            bufferInt = atoi(buffer);
+      //            if(bufferInt >= minimo && bufferInt<= maximo)
+       //           {
+        //              *resultado = bufferInt;
+         //             retorno = 0
+          //        }
+           //
+            //  }
+             // else
+                {
+
+
+                }
+
+
+            }
+
+
+
+
+            return retorno;
+
+
+
+
+        }
+
+
+
+
 
     // int utn_getString(char *msg, char *msgEror,int minimo, int maximo,int reintentos,char *resultado)
-    //
+    // {
+
+    //    strncpy(resultado, "1234",5);
+     //   return 0;
+
+     // }
 
 
     //
